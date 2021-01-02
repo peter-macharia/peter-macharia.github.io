79cc220e8fc72a476d853a166f81422cea8dc3cf
peter-macharia <mainapter081@gmail.com>
2021-01-02 15:59:38 +0300

    switch from custome theme to Minimal Mistakes theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "What to expect",
+        "excerpt":"Going to be sharing my notes and other security related materials. Topics to be covered will include:      Linux fundamentals   Bash scripting   Python For hackers   Automation using python and bash   Web hacking fundamentals   Mobile hacking fundamentals(Android)   CTF writeups(hopefully)   Usage of common hacking tools   ","categories": ["blog"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/2021/01/02/content/",
+        "teaser":null},]
